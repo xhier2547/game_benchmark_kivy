bf6c343096979dc36e35581f9f9f2d67d238ca15
Xhi3r <aphilakjankaewdach@gmail.com>
2024-01-08 23:14:59 +0700

    add score rank, about, back to menu

--- game/main.py
+++ game/main.py
@@ -5,25 +5,37 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 from random import uniform
 
+
 class MenuScreen(BoxLayout):
-    def __init__(self, start_callback, **kwargs):
+    def __init__(self, start_callback, show_score_rank_callback, show_about_callback, **kwargs):
         super(MenuScreen, self).__init__(**kwargs)
         self.orientation = "vertical"
         self.spacing = 10
 
-        self.start_button = Button(text="Start Reaction Test", on_press=start_callback, font_size = '70sp')
-        self.start_button.background_color =(200/255, 70/255, 90/255, 1)
+        self.start_button = Button(text="Start Reaction Test", on_press=start_callback, font_size='70sp')
+        self.start_button.background_color = (200/255, 70/255, 90/255, 1)
         self.add_widget(self.start_button)
 
+        self.score_rank_button = Button(text="Score Rank", on_press=show_score_rank_callback, font_size='20sp')
+        self.score_rank_button.background_color = (100/255, 100/255, 255/255, 1)
+        self.add_widget(self.score_rank_button)
+
+        self.about_button = Button(text="About", on_press=show_about_callback, font_size='20sp')
+        self.about_button.background_color = (100/255, 100/255, 255/255, 1)
+        self.add_widget(self.about_button)
+
 class ReactionTimeGame(BoxLayout):
-    def __init__(self, reset_callback, **kwargs):
+    def __init__(self, reset_callback, save_callback, back_to_menu_callback, **kwargs):
         super(ReactionTimeGame, self).__init__(**kwargs)
         self.orientation = "vertical"
         self.spacing = 0
 
         self.reset_callback = reset_callback
+        self.save_callback = save_callback
+        self.back_to_menu_callback = back_to_menu_callback
+        self.reaction_times = []
 
-        self.reaction_box = Button(text="Click me!", font_size = '60sp', on_press=self.record_reaction_time)
+        self.reaction_box = Button(text="Click me!", font_size='60sp', on_press=self.record_reaction_time)
         self.reaction_box.background_color = (1/255, 230/255, 50/255, 1)
         self.reaction_box.opacity = 0
         self.add_widget(self.reaction_box)
@@ -31,10 +43,19 @@ class ReactionTimeGame(BoxLayout):
         self.reaction_time_label = Label(text="")
         self.add_widget(self.reaction_time_label)
 
-        self.reset_button = Button(text="Try again", font_size = '60sp', on_press=self.reset_test)
+        self.reset_button = Button(text="Try again", font_size='60sp', on_press=self.reset_test)
         self.reset_button.background_color = (100/255, 100/255, 255/255, 1)
         self.reset_button.opacity = 0
         self.add_widget(self.reset_button)
+
+        self.save_button = Button(text="Save Score", font_size='20sp', on_press=self.save_score)
+        self.save_button.background_color = (100/255, 100/255, 255/255, 1)
+        self.save_button.opacity = 0
+        self.add_widget(self.save_button)
+
+        self.back_to_menu_button = Button(text="Back to Menu", font_size='20sp', on_press=self.back_to_menu)
+        self.back_to_menu_button.background_color = (100/255, 100/255, 255/255, 1)
+        self.add_widget(self.back_to_menu_button)
 
         self.num_attempts = 0
         self.total_reaction_time = 0
@@ -47,7 +68,7 @@ class ReactionTimeGame(BoxLayout):
 
     def change_color(self, dt):
         self.reaction_box.disabled = False
-        self.reaction_box.background_color = (00/255, 560/255, 150/255, 1)
+        self.reaction_box.background_color = (0/255, 240/255, 100/255, 1)
         self.start_time = Clock.get_time()
 
     def record_reaction_time(self, instance):
@@ -63,35 +84,77 @@ class ReactionTimeGame(BoxLayout):
                 Clock.schedule_once(self.start_test)
             else:
                 self.display_average_reaction_time()
-                
+
     def display_average_reaction_time(self):
         average_reaction_time = self.total_reaction_time / self.num_attempts
-        self.reaction_time_label.text =f"Average Reaction Time: {average_reaction_time:.0f} ms" 
+        self.reaction_time_label.text = f"Average Reaction Time: {average_reaction_time:.0f} ms"
         self.reaction_time_label.font_size = '40sp'
         self.reset_button.opacity = 1
-        
+        self.save_button.opacity = 1
+        self.reaction_times.append(average_reaction_time)
+
     def reset_test(self, instance):
         self.reaction_box.disabled = False
         self.reaction_time_label.text = ""
         self.num_attempts = 0
         self.total_reaction_time = 0
         self.reset_button.opacity = 0
-        self.reset_callback() 
+        self.save_button.opacity = 0
+        self.reset_callback()
+
+    def save_score(self, instance):
+        self.save_callback(instance)
+
+    def back_to_menu(self, instance):
+        self.back_to_menu_callback()
 
 class ReactionTimeTestApp(App):
     def build(self):
-        self.menu_screen = MenuScreen(start_callback=self.start_game)
+        self.menu_screen = MenuScreen(
+            start_callback=self.start_game,
+            show_score_rank_callback=self.show_score_rank,
+            show_about_callback=self.show_about
+        )
         return self.menu_screen
 
     def start_game(self, instance):
-        self.game_screen = ReactionTimeGame(reset_callback=self.reset_game)
+        self.game_screen = ReactionTimeGame(
+            reset_callback=self.reset_game,
+            save_callback=self.save_score,
+            back_to_menu_callback=self.back_to_menu
+        )
         self.menu_screen.clear_widgets()
         self.menu_screen.add_widget(self.game_screen)
         self.game_screen.start_test()
 
     def reset_game(self):
         self.menu_screen.clear_widgets()
-        self.menu_screen.add_widget(MenuScreen(start_callback=self.start_game))
+        self.menu_screen.add_widget(MenuScreen(
+            start_callback=self.start_game,
+            show_score_rank_callback=self.show_score_rank,
+            show_about_callback=self.show_about
+        ))
+
+    def save_score(self, instance):
+        if hasattr(self, 'game_screen') and isinstance(self.game_screen, ReactionTimeGame):
+            self.game_screen.save_score(instance)
+
+    def back_to_menu(self):
+        self.menu_screen.clear_widgets()
+        self.menu_screen.add_widget(MenuScreen(
+            start_callback=self.start_game,
+            show_score_rank_callback=self.show_score_rank,
+            show_about_callback=self.show_about
+        ))
+
+    def show_score_rank(self, instance):
+        # รอใส่ฟังก์ชั่น
+        pass
+
+    def show_about(self, instance):
+        # รอใส่ฟังก์ชั่น
+        pass
+
 
 if __name__ == '__main__':
     ReactionTimeTestApp().run()
